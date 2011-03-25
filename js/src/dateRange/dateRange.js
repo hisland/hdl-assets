59df521a5de4a45a221d3d92669c1972c5350b7b
hisland <hisland@qq.com>
2011-03-25 18:53:22 +0800

    hdlDateRange 更名为 dateRange 并修改基础代码

---
+++ js/src/dateRange/dateRange.js
@@ -0,0 +1,17 @@
+/**********************************************************************************************
+ * 名称: 日期范围控件
+ * 作者: hisland
+ * 邮件: hisland@qq.com
+ * 时间: @TIMESTAMP@
+ * 版本: @VERSION@
+ * 
+ */
+
+KISSY.add('dateRange', function(S, undef) {
+	var  $ = jQuery
+		,EMPTY_$ = $('');
+
+
+}, {
+	requires: ['jquery-1.4.2', 'adjustElement']
+});
