2b05028a4af1c20cc6c9b6166838598bcc1e5e4c
hisland <hisland@qq.com>
2011-03-31 17:51:12 +0800

    weekTool 第2版代码基本完成

--- js/src/weekTool/weekTool.js
+++ js/src/weekTool/weekTool.js
@@ -19,14 +19,15 @@
  */
 
 KISSY.add('weekTool', function(S, undef) {
-
-	function weekTool(year){
-		if(year !== undefined){
-			return new init(year);
-		}else{
-			return new init(new Date().getFullYear());
-		}
-	}
+	var  $ = jQuery
+		,EMPTY_$ = $('')
+		,ipt_now = EMPTY_$
+		,ipt_start = EMPTY_$
+		,ipt_end = EMPTY_$
+		,div_pop = EMPTY_$
+		,a_year = EMPTY_$
+		,div_year_list = EMPTY_$
+		,div_list = EMPTY_$;
 
 	function init(y){
 		this.year(y);
@@ -67,7 +68,178 @@ KISSY.add('weekTool', function(S, undef) {
 		}
 	});
 
-	$.weekTool = weekTool;
+	function makeList(year){
+		var  w = new init(year)
+			,b = []
+			,i = 1, t;
+
+		//按自然周算,一年正常有53周,最多有54周,最后一周根据年相同与否来判断
+		for(; i<=54; i++){
+			if(i<54 || w.week(i).start().getFullYear() == year){
+				w.week(i);
+				b.push('<li><span class="week-tool-col1">', i, '</span><a href="#" class="week-tool-col2">');
+				t = w.start();
+				b.push(t.dateTimeString());
+				b.push('</a><a href="#" class="week-tool-col3">');
+				t = w.end();
+				b.push(t.dateTimeString());
+				b.push('</a></li>');
+			}
+		}
+
+		div_list.html(b.join(''));
+	}
+	function makeYearList(from, to){
+		var  b = [];
+		for(; from<to; from++){
+			b.push('<a href="#" class="week-tool-yearli">', from, '</a> ');
+		}
+		div_year_list.html(b.join(''));
+	}
+
+	//公共显示隐藏函数
+	function popShow(){
+		//需要时再加载此层
+		if(!div_pop.length){
+			div_pop = $('<div class="week-tool-wrap"><div class="week-tool-yearw"><a href="#" class="week-tool-yearwp">&lt;</a><a href="#" class="week-tool-year">2011</a><a href="#" class="week-tool-yearwn">&gt;</a></div><div class="week-tool-yearp"><div class="week-tool-yearl"></div><div class="week-tool-yearlp"><a href="#" class="week-tool-yearlpp">上一页</a><a href="#" class="week-tool-yearlpn">下一页</a></div></div><div class="week-tool-head"><span class="week-tool-col1">周</span><span class="week-tool-col2">开始时间</span><span class="week-tool-col3">结束时间</span></div><ul class="week-tool-list"></ul></div>');
+			div_pop.click(popClick).dblclick(popDblClick);
+			a_year = div_pop.find('a.week-tool-year');
+			div_year_list = div_pop.find('div.week-tool-yearl');
+			div_list = div_pop.find('ul.week-tool-list');
+			div_pop.appendTo('body');
+		}
+		div_pop.show().adjustElement(ipt_now);
+	}
+	function popHide(){
+		div_year_list.parent().hide();
+		div_pop.hide();
+	}
+
+	//输入框的一系列事件
+	function iptFocus(e){
+		var other;
+		if(ipt_now[0] != this){
+			ipt_now = $(this);
+			if(other = ipt_now.attr('data-week-start')){
+				ipt_start = $(other);
+				ipt_end = ipt_now;
+			}else if(other = ipt_now.attr('data-week-end')){
+				ipt_start = ipt_now;
+				ipt_end = $(other);
+			}else{
+				ipt_start = ipt_end = EMPTY_$;
+			}
+			popShow();
+			makeList(2011);
+		}else{
+			popShow();
+		}
+	}
+	function popClick(e){
+		var  t = e.target
+			,dt = $(t)
+			,li_p, val;
+
+		if(dt.is('a')){
+			li_p = dt.parent();
+			if(dt.is('.week-tool-col2')){//开始时间
+				li_p.prevAll('.week-tool-selected').removeClass('week-tool-selected');
+				if(li_p.nextAll('.week-tool-selected').length){
+					li_p.nextUntil('.week-tool-selected').andSelf().addClass('week-tool-selected');
+					ipt_start.val(dt.text());
+				}else{
+					li_p.addClass('week-tool-selected');
+					ipt_start.val(dt.text());
+					ipt_end.val(dt.next().text());
+				}
+			}else if(dt.is('.week-tool-col3')){//结束时间
+				li_p.nextAll('.week-tool-selected').removeClass('week-tool-selected');
+				if(li_p.prevAll('.week-tool-selected').length){
+					li_p.prevUntil('.week-tool-selected').andSelf().addClass('week-tool-selected');
+					ipt_end.val(dt.text());
+				}else{
+					li_p.addClass('week-tool-selected');
+					ipt_end.val(dt.text());
+					ipt_start.val(dt.prev().text());
+				}
+			}else if(dt.is('.week-tool-yearwp')){//年-
+				val = a_year.text()-1;
+				a_year.html(val);
+				makeList(val);
+			}else if(dt.is('.week-tool-yearwn')){//年+
+				val = a_year.text()-0+1;
+				a_year.html(val);
+				makeList(val);
+			}else if(dt.is('.week-tool-year')){//点击显示年
+				val = dt.text()-0;
+				makeYearList(val-12, val+12);
+				div_year_list.parent().show();
+			}else if(dt.is('.week-tool-yearli')){//年列表
+				val = dt.text()-0;
+				a_year.html(val);
+				makeList(val);
+			}else if(dt.is('.week-tool-yearlpp')){//年列表上一页
+				val = div_year_list.find('a:first').text()-0;
+				makeYearList(val-24, val);
+			}else if(dt.is('.week-tool-yearlpn')){//年列表下一页
+				val = div_year_list.find('a:last').text()-0;
+				makeYearList(val+1, val+25);
+			}
+			dt.blur();
+			e.preventDefault();
+		}
+
+		//内部隐藏年列表层
+		if(!(dt.is('a.week-tool-year') || dt.closest('.week-tool-yearp').length)){
+			div_year_list.parent().hide();
+		}
+	}
+	function popDblClick(e){
+		if($(e.target).is('a.week-tool-col2, a.week-tool-col3')){
+			popHide()
+		}
+	}
+
+	function isWeekInput(elm){
+		var attr = elm.getAttribute('type');
+		if(elm.tagName.toUpperCase() === 'INPUT' && attr === 'week'){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	//注册事件,可用于手工注册
+	function weekTool(){
+		this.each(function(i, v){
+			if(!v.__bind_week_tool){
+				v.__bind_week_tool = true;
+
+				$(v).focus(iptFocus);
+			}
+		});
+		return this;
+	}
+
+	//文档上监听并注册事件
+	function documentClick(e){
+		var  t = e.target
+			,dt = $(t);
+		//如已注册则忽略
+		if(!t.__bind_week_tool && isWeekInput(t)){
+			dt.weekTool();
+			dt.focus();
+		}
+		//顺带做隐藏操作
+		if(!dt.closest('.week-tool-wrap').length && !isWeekInput(t)){
+			popHide();
+		}
+	}
+	$(document).click(documentClick);
+
+	$.fn.extend({
+		weekTool: weekTool
+	});
 }, {
-	requires: ['jquery-1.4.2']
+	requires: ['jquery-1.4.2', 'adjustElement']
 });
