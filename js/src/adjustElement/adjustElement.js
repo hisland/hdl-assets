d07c9378ae1c8a1198eeb5ba1b7da2f698e4a7bf
hisland <hisland@qq.com>
2011-05-06 11:02:25 +0800

    adjustElement 增加 offset 和 强制/自由 2种对齐方式

--- js/src/adjustElement/adjustElement.js
+++ js/src/adjustElement/adjustElement.js
@@ -6,8 +6,8 @@
  * 版本: v2
  * 
  * 2011-4-12 15:3:54:
- *		元素设置data-offset属性使对齐可以加一定的偏移
- *		元素设置data-adjust-type属性使强制左/右/上/下对齐或者以显示完全为主要对齐(某些情况强制对齐一边浮动元素都不能显示全)
+ *		元素设置data-offset="x, y", 默认[0, 0], 属性使对齐可以加一定的偏移
+ *		元素设置data-adjust-type="force", 默认[view], 属性使强制左/右/上/下对齐[force]或者以显示完全为主要对齐[view](某些情况强制对齐一边浮动元素都不能显示全)
  *
  * 使用方法:
  *			$('').adjustElement(target);
@@ -19,7 +19,7 @@ KISSY.add('adjustElement', function(S, undef) {
 
 	function adjustElement(target, callback){
 		var elm = this.eq(0);
-		target = $(target);
+		target = $(target).eq(0);
 
 		if(!target.length){
 			alert('adjustElement: 必须有个 target');
@@ -28,6 +28,8 @@ KISSY.add('adjustElement', function(S, undef) {
 
 		var  me_width = elm.outerWidth()
 			,me_height = elm.outerHeight()
+			,offset = [0, 0]
+			,tmp
 
 			,t_width = target.outerWidth()
 			,t_height = target.outerHeight()
@@ -36,22 +38,53 @@ KISSY.add('adjustElement', function(S, undef) {
 			,p_width = document.documentElement.clientWidth
 			,p_height = document.documentElement.clientHeight;
 
-		//设置水平位置
-		if(me_width + t_offset.left > p_width){
-			elm.css('left', p_width - me_width - 5)//窗口右边线对齐
-		}else{
-			elm.css('left', t_offset.left);//target左对齐
-		}
+		//得到偏移
+		$.each((elm.attr('data-offset')+'').split(/[,\s]+/), function(i, v){
+			v -= 0;
+			if(!isNaN(v)){
+				offset[i] = v;
+			}
+		});
 
-		//设置垂直位置
-		if(me_height + t_offset.top > p_height){
-			elm.css('top', t_offset.top - me_height -1)//target顶边对齐
+		//强制左/右/上/下对齐
+		if(elm.attr('data-adjust-type') == 'force'){
+			//设置水平位置
+			if(t_offset.left + offset[0] + me_width > p_width){
+				elm.css('left', p_width - me_width - 5);//窗口右边线对齐
+			}else{
+				elm.css('left', t_offset.left + offset[0]);//target左对齐
+			}
+
+			//设置垂直位置
+			if(me_height + offset[1] + t_offset.top > p_height){
+				elm.css('top', t_offset.top - me_height -1);//target顶边对齐
+			}else{
+				elm.css('top', t_offset.top + offset[1] + t_height + 1);//target底边对齐
+			}
+		
+		//以显示完全为主要
 		}else{
-			elm.css('top', t_offset.top + t_height + 1);//target底边对齐
+			//设置水平位置
+			if(me_width > p_width){
+				elm.css('left', 0);//窗口左边线对齐
+			}else if(t_offset.left + offset[0] + me_width > p_width){
+				elm.css('left', p_width - me_width - 5);//窗口右边线对齐
+			}else{
+				elm.css('left', t_offset.left + offset[0]);//target左对齐
+			}
+
+			//设置垂直位置
+			if(me_height > p_height){
+				elm.css('top', 0);//窗口顶边线对齐
+			}else if(me_height + offset[1] + t_offset.top > p_height){
+				elm.css('top', p_height - me_height -1);//窗口底边线对齐
+			}else{
+				elm.css('top', t_offset.top + offset[1] + t_height + 1);//target底边对齐
+			}
 		}
 
 		//有回调的回调
-		$.isFunction(callback) && callback(elm);
+		$.isFunction(callback) && callback.call(elm);
 
 		return elm;
 	}
