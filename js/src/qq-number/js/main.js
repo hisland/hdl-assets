6cd47ef68d0a68c46ff53dea40bdd0b400ce371b
hisland <hisland@qq.com>
2011-05-16 17:29:07 +0800

    开通年会员 qq号码选择工具

--- js/src/qq-number/js/main.js
+++ js/src/qq-number/js/main.js
@@ -5,7 +5,8 @@ $(function(){
 	var  page_from = 0
 		,page_to = 50
 
-		,qq_list = [];
+		,qq_list = {}
+		,num_sort = [];
 
 	function getList(page, callback){
 		$.get('http://haoma.qq.com/static/asset/asset_index_'+page+'.html', function(rs){
@@ -13,10 +14,7 @@ $(function(){
 			var rs2 = rs.replace(/<!--\[if !IE\]>.*<!\[endif\]-->$/, '');
 			rs2 = $.parseJSON(rs2);
 			$('a', rs2.numbers).each(function(i, v){
-				qq_list.push({
-					 num: $(v).text()
-					,url: $(v).attr('href')
-				});
+				qq_list[$(v).text()] = $(v).attr('href');
 			});
 		});
 	}
@@ -27,13 +25,28 @@ $(function(){
 
 	$('.ctrl-p:eq(0) :button').click(function(e){
 		var text = $(this).prev().val();
+		var num = [];
+		$.each(qq_list, function(i, v){
+			if((v.num+'').indexOf(text) != -1){
+				num.push(v);
+			}
+		});
 	});
-	$('.ctrl-p:eq(1) :button').click(function(e){
-		var text = $(this).prev().val();
-		$(this);
+	//导出到文件
+	$('#export').click(function(e){
+		$.each(qq_list, function(i, v){
+			num_sort.push(i);
+		});
+		num_sort.sort();
+		var c = [];
+		$.each(num_sort, function(i, v){
+			c.push('\n', v, '\t', qq_list[v]);
+		});
+		var f = file('qq-' + (+new Date) + 'qq.txt');
+		f.save(c.join(''));
 	});
 	//获取50页号码
-	$('.ctrl-p2 :button').click(function(e){
+	$('#get-num').click(function(e){
 		var i = page_from;
 		for(; i<page_to; i++){
 			getList(i);
