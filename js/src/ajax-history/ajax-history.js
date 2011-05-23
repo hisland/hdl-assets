7e25ad0dac7622d059cef5af98d4820e896a1863
hisland <hisland@qq.com>
2011-05-24 07:52:19 +0800

    新增 ajax-history 模块

---
+++ js/src/ajax-history/ajax-history.js
@@ -0,0 +1,22 @@
+/**********************************************************************************************
+ * 名称: 异步历史记录
+ * 作者: hisland
+ * 邮件: hisland@qq.com
+ * 时间: 2011-5-24 7:51:42
+ * 版本: v1.0
+ * 
+ */
+
+
+KISSY.add('ajax-history', function(S, undef) {
+	var  $ = jQuery
+		,EMPTY_$ = $('');
+
+	function ajaxHistory(){
+		
+	}
+
+	window.ajaxHistory = ajaxHistory;
+}, {
+	requires: ['jquery-1.4.2']
+});
