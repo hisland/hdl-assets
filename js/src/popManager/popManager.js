ce5f63b5b5bbe05896983e2046295ee68d39149c
hisland <hisland@qq.com>
2011-05-28 16:59:27 +0800

    修改 popManager, popWin 模块

--- js/src/popManager/popManager.js
+++ js/src/popManager/popManager.js
@@ -4,45 +4,52 @@
  * 邮件: hisland@qq.com
  * 时间: @TIMESTAMP@
  * 版本: @VERSION@
- *
+ * 
  * API:
- *	popManager.clean() 清除所有的弹出层包含块
- *	p = popManager.init() 初始化一个弹出层包含块
- *	p.front() 将此层放到最前面
- *	p.mask() 使用遮罩--css3使用半透明背景,否则使用半透明层
- *	p.remove() 删除此层
+ * 		$.popManager.clean() 清除所有的弹出层包含块
+ * 		p = $.popManager.init() 初始化一个弹出层包含块
+ * 		p.front() 将此层放到最前面
+ * 		p.mask() 使用遮罩
+ * 		p.mask(false) 去除遮罩
+ * 		p.remove() 删除此层
+ * 
+ * 		p.div 最外层元素的jquery对象
+ * 		p.div.appendTo('body') 将层放到dom树中
  */
 
 KISSY.add('popManager', function(S, undef) {
 	var  $ = jQuery
 		,EMPTY_$ = $('')
 		,base_z_index = 3000
-		,html_string = '<div style="position:absolute;top:0;left:0;width:100%;height:100%;"></div>'
-		,mask_string = '<div style="position:absolute;top:0;left:0;width:100%;height:100%;background-color:#fff;opacity:0.8;filter:alpha(opacity=80);"></div>'
+		,html_string = '<div style="position:absolute;top:0;left:0;width:100%;height:100%;display:none;"></div>'
+		,mask_string = '<div style="position:absolute;top:0;left:0;width:100%;height:100%;background-color:#000;filter:alpha(opacity=20);"></div>'
 		,m = {}
 		
 		,_uid = 0
 		,uid = function(){
 			return (++_uid + base_z_index);
 		};
-	
+
 	m.divs = EMPTY_$;
+
+	//初始化一个弹出层包含块
 	m.init = function(){
-		var o = new init();
-		o.front();
-		return o;
+		return new init().front();
 	}
+
+	//清除所有的弹出层包含块
 	m.clean = function(){
 		m.divs.remove();
 		m.divs = EMPTY_$;
 		_uid = 0;
+		return this;
 	}
 
 	function init(){
 		this.div = $(html_string);
 		m.divs = m.divs.add(this.div);
 	}
-	init.prototype = {
+	$.extend(init.prototype, {
 		//放到最前
 		 front: function () {
 			this.div.css('z-index', uid());
@@ -51,10 +58,30 @@ KISSY.add('popManager', function(S, undef) {
 		//删除此弹出层
 		,remove: function () {
 			this.div.remove();
+			return this;
 		}
-	}
+		//遮罩处理,css3使用半透明背景,否则使用半透明层
+		,mask: function (use) {
+			if(use == false){
+				if($.browser.msie){
+					this.div.children('div:first').remove();
+				}else{
+					this.div.css('background-color', '');
+				}
+			}else{
+				if($.browser.msie){
+					this.div.prepend(mask_string);
+				}else{
+					this.div.css('background-color', 'rgba(0, 0, 0, 0.2)');
+				}
+			}
+			return this;
+		}
+	});
 
-	window.popManager = m;
+	$.extend({
+		popManager: m
+	});
 }, {
 	requires: ['jquery-1.4.2']
 });
--- js/src/popWin/popWin.js
+++ js/src/popWin/popWin.js
@@ -3,6 +3,19 @@
  * 作者: hisland
  * 邮件: hisland@qq.com
  * 时间: 2011-2-17 16:28:10
+ * 
+ * API:
+ * 		$.popWin.clean() 清除所有的弹出层
+ * 
+ * 		var p = $.popWin.init() 初始化一个弹出层
+ * 		p.front() 将此层放到最前面
+ * 		p.mask() 使用遮罩
+ * 		p.mask(false) 去除遮罩
+ * 		p.remove() 删除此层
+ * 
+ * 		p.div 最外层元素的jquery对象
+ * 		p.show() 显示弹出窗口
+ * 		p.hide() 隐藏弹出窗口
  */
 
 KISSY.add('popWin', function(S, undef) {
@@ -20,51 +33,72 @@ KISSY.add('popWin', function(S, undef) {
 	}
 
 	function init(){
-		var self = this;
-		this.div = $(html_string);
-		popWin.divs = popWin.divs.add(this.div);
-		this.close = this.div.find('a.win1-close');
-		this.title = this.div.find('span.win1-title');
-		this.content = this.div.find('div.win1-content');
-		this.btn_ok = this.div.find('input.win1-btn-ok');
-		this.btn_cancle = this.div.find('input.win1-btn-cancle');
-		this.close_able = true;
+		var  self = this
+			,div = $(html_string);
 
-		this.mask = $.mask.init();
-		this.manager = popManager.init();
-		this.manager.div.append(this.mask.div.show()).append(this.div).hide().appendTo('body');
-		this.div.width(500);
-		this.div.hdlDrag();
+		//保存到全局
+		popWin.divs = popWin.divs.add(div);
 
-		this.close.add(this.btn_cancle).click(function(e){
+		//设置属性
+		self.div = div;
+		self.close = div.find('a.win1-close');
+		self.title = div.find('span.win1-title');
+		self.content = div.find('div.win1-content');
+		self.btn_ok = div.find('input.win1-btn-ok');
+		self.btn_cancle = div.find('input.win1-btn-cancle');
+		self.close_able = true;
+
+		self.manager = $.popManager.init().mask();
+		self.manager.div.append(div).appendTo('body');
+		self.div.width(400);
+		self.div.hdlDrag();
+
+		//设置关闭按钮
+		self.close.add(self.btn_cancle).click(function(e){
 			self.hide();
 			e.preventDefault();
 		}).mousedown(function(e){
-			return false;
-		});
-
-		this.content.mousedown(function(e){
 			e.stopPropagation();
 		});
 
-	}
-	init.prototype.show = function(){
-		this.manager.div.show();
-		this.div.css({
-			 top: (document.documentElement.clientHeight-this.div.height())/2
-			,left:(document.documentElement.clientWidth-this.div.width())/2
+		//内容块不能拖动
+		self.content.mousedown(function(e){
+			e.stopPropagation();
 		});
 	}
-	init.prototype.hide = function(){
-		if(this.close_able){
-			this.manager.div.hide();
-		}
-	}
-	init.prototype.remove = function(){
-		this.manager.div.remove();
-	}
 
-	$.popWin = popWin;
+	$.extend(init.prototype, {
+		 front: function(){
+			this.manager.front();
+			return this;
+		}
+		,mask: function(use){
+			this.manager.mask(use);
+			return this;
+		}
+		,show: function(){
+			this.manager.div.show();
+			this.div.css({
+				 top: (document.documentElement.clientHeight-this.div.height())/2
+				,left:(document.documentElement.clientWidth-this.div.width())/2
+			});
+			return this;
+		}
+		,hide: function(){
+			if(this.close_able){
+				this.manager.div.hide();
+			}
+			return this;
+		}
+		,remove: function(){
+			this.manager.div.remove();
+			return this;
+		}
+	});
+
+	$.extend({
+		popWin: popWin
+	});
 }, {
-	requires: ['popManager', 'hdlDrag']
+	requires: ['jquery-1.4.2', 'popManager', 'hdlDrag']
 });
