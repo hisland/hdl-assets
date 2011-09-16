5c45de1aeb8fe9f214ca580dbfacd2f7210af0d6
hisland <hisland@qq.com>
2011-09-16 18:54:26 +0800

    表格控件更新

--- js/src/hdlGrid/hdlGrid.js
+++ js/src/hdlGrid/hdlGrid.js
@@ -10,37 +10,44 @@
  * 		g.setting(setting); - 修改设置
  * 		g.data(data); - 修改内容
  * 		g.refresh(); - 刷新表格显示
- * 		
- *      g.addCol(col_set); - 添加一列
+ * 
+ *		g.addCol(col_set); - 添加一列
  * 		g.setCol(n, [fn|col_set]); - 修改n列, 传入修改函数或者设置
  * 		g.addRow(data); - 添加一行
  * 		g.setRow(n, fn); - 修改n行, 传入修改函数
- * 		
+ * 
  * 		g.div - 指向表格的最外层元素,为一个jq对象
  * 
- * 2011-4-23 16:35:4:
- * 		3步走:
- * 			1.初始化一个空壳
- * 			2.填充数据
- * 			3.修正宽高
- * 			3.drag位置及初始化[如果有]
- * 
- * 		表格生成之后可以:
- * 			1.增加行/列
- * 			2.修改行/列
- * 			3.遍历行/列
- * 			4.修改设置[grid, col, row]
- * 			5.嵌入html要注意html实体的转义,除非特殊指明使用原始数据
- * 
- * 2011-06-20 16:43:51:
- * 		single_check, chekckbox只能选择一个, 没有全选checkbox, 也就是一次只能操作1行数据
- * 		动态改变列
- * 		动态改变行
- * 		checkbox
- * 		编辑
- * 		scrollbar_width
- * 		合并单元格
- * 
+
+2011-4-23 16:35:4:
+	3步走:
+		1.初始化一个空壳
+		2.填充数据
+		3.修正宽高
+		3.drag位置及初始化[如果有]
+
+	表格生成之后可以:
+		1.增加行/列
+		2.修改行/列
+		3.遍历行/列
+		4.修改设置[grid, col, row]
+		5.嵌入html要注意html实体的转义,除非特殊指明使用原始数据
+
+2011-06-20 16:43:51:
+	single_check, chekckbox只能选择一个, 没有全选checkbox, 也就是一次只能操作1行数据
+	动态改变列
+	动态改变行
+	checkbox
+	编辑
+	scrollbar_width
+	合并单元格
+
+2011-07-21 11:02:27:
+	按住shift键选择范围
+
+2011-09-02 14:17:37:
+	动态增加删除列
+	动态隐藏显示列
  */
 
 KISSY.add('hdlGrid', function(S, undef) {
@@ -68,9 +75,9 @@ KISSY.add('hdlGrid', function(S, undef) {
 			,enable_page: true			//是否使用分页
 			,page_curr: 0				//当前页
 			,var_page: 'page'			//page_curr的name, 对应后台接收
-			,page_total: 0				//总页数
-			,num_per_page: [15, [5, 10, 15, 20]]//[0]为每页显示数量, [1]为可选择的页数量值
 			,var_num_per_page: 'perPageNum'	//每页显示数量的name, 对应后台接收
+			,page_total: 0				//总页数
+			,num_per_page: [2, [5, 10, 15, 20]]//[0]为[1]的索引, [1]为可选择的页数量值
 
 			,url: ''					//获取数据的地址
 			,param: ''					//传递的额外参数, 可为函数, 函数命名空间[暂不支持]
@@ -79,8 +86,8 @@ KISSY.add('hdlGrid', function(S, undef) {
 
 			,single_row: true			//文本强制在一行
 
-			,single_check: false		//单行选中checkbox
-			,single_select: false		//单行选择
+			,single_check: false		//单行选中checkbox, 无全选
+			,single_select: true		//单行选择
 
 			,msg_empty: '暂无数据,根据内容填充后的大小居中显示'	//无数据时显示的内容
 			,msg_proc: '正在加载,请稍等...'	//加载时显示的内容
@@ -139,7 +146,9 @@ KISSY.add('hdlGrid', function(S, undef) {
 
 		return grid;
 	}
-	$.extend(Grid.prototype, {
+
+	//设置原型方法
+	S.augment(Grid, {
 		//初始化table基本结构,并设置引用
 		__initTable: function(){
 			var div = $('<div class="hdlgrid-wrap"><div class="hdlgrid-head"><div class="hdlgrid-head-in"><table><thead><tr></tr></thead></table></div></div><div class="hdlgrid-body"><div class="hdlgrid-body-in"><table><tbody></tbody></table></div></div><div class="hdlgrid-pager"><span class="hdlgrid-prev hdlgrid-prev-gray"></span><span class="hdlgrid-next hdlgrid-next-gray"></span><span class="hdlgrid-first hdlgrid-first-gray"></span><span class="hdlgrid-last hdlgrid-last-gray"></span><span class="hdlgrid-sep"></span><span class="hdlgrid-text"></span><span class="hdlgrid-sum"></span></div><div class="hdlgrid-resizer"><div class="hdlgrid-resizer-i"></div></div><div class="hdlgrid-toggle-div"><table><tbody><tr><td><input type="checkbox" /></td><td></td></tr></tbody></table></div><div class="hdlgrid-toggle-btn"></div><div class="hdlgrid-mask"></div><div class="hdlgrid-nodata"></div><div class="hdlgrid-loading"></div></div>');
@@ -163,16 +172,20 @@ KISSY.add('hdlGrid', function(S, undef) {
 			return this;
 		}
 
-		//添加一列定义
+		//添加一列数据
 		,addCol: function(col_setting){
 			S.mix(col_setting, pre_col_model, false);//不覆盖相同的设置,只copy不存在的设置
 			this.div.thead.find('tr').append('<th>'+ col_setting.display+'</th>');
+			return this;
 		}
-		//修改一列定义
+
+		//修改一列数据
 		,setCol: function(n, fn){
 			
+			return this;
 		}
-		//交换两个索引指向的列,统一的从0开始计数
+
+		//交换两个索引指向的列,从0开始计数
 		,swapCol: function(n1, n2){
 			//n1小, n2大
 			var tmp = [n1, n2].sort();
@@ -203,52 +216,74 @@ KISSY.add('hdlGrid', function(S, undef) {
 
 			return this;
 		}
+
 		//添加一行数据
 		,addRow: function(row_data){
 			
+			return this;
 		}
+
 		//修改一行数据
 		,setRow: function(n, fn){
 			
+			return this;
+		}
+
+		//交换两个索引指向的行,从0开始计数
+		,swapRow: function(n1, n2){
+			return this;
 		}
 
 		//遍历表头
 		//fn: this->grid, (th, idx)
 		,walkHead: function(fn){
 			
+			return this;
 		}
+
 		//遍历某行的单元格
 		//fn: this->grid, (td, idx)
 		,walkCell: function(row_n, fn){
 			
+			return this;
 		}
+
 		//遍历列
 		//fn: this->grid, (th, td, idx)
 		,walkCol: function(col_n, fn){
 			
+			return this;
 		}
+
 		//遍历行
 		//fn: this->grid, (tr, idx)
 		,walkRow: function(fn){
 			
+			return this;
 		}
 
 		//修改设置
 		,setting: function(setting){
 			
+			return this;
 		}
+
 		//修改数据
 		,data: function(data){
 			
+			return this;
 		}
 
 		//生成/更新拖动条
 		,makeDrag: function(){
 			
+			return this;
 		}
+
 		//更新拖动条位置
 		,fixDragPos: function(){
 			
+			return this;
 		}
 
 		//更新宽高
@@ -276,6 +311,8 @@ KISSY.add('hdlGrid', function(S, undef) {
 			}
 
 			headnbody.css('table-layout', 'fixed');
+
+			return this;
 		}
 
 		//获得显示的列名称,以标准键值对返回
@@ -283,28 +320,37 @@ KISSY.add('hdlGrid', function(S, undef) {
 			var_name = var_name || 'colName';
 
 			var param = [];
+			return param;
 		}
 
 		//刷新显示
 		,refresh: function(){
 			
 		}
-		//遮罩的显示与否
-		,loading: function(status){
-			if(status){
-				this.div.mask.add(this.div.loading).show();
-			}else{
-				this.div.mask.add(this.div.loading).hide();
-			}
+
+		//显示遮罩
+		,loading: function(){
+			this.div.mask.add(this.div.loading).show();
+			return this;
 		}
+
+		//隐藏遮罩
+		,loaded: function(){
+			this.div.mask.add(this.div.loading).hide();
+			return this;
+		}
+
 		//无数据显示文本
 		,noData: function(str){
 			this.div.nodata.html(str || this.msg_empty);
+			return this;
 		}
+
 		//转到某页
 		,goPage: function(n){
-			
+			return this;
 		}
+
 		//刷新显示
 		,ajaxLoad: function(fn){
 			var grid = this;
@@ -329,6 +375,7 @@ KISSY.add('hdlGrid', function(S, undef) {
 				grid.loading(0);
 			});
 		}
+
 		//将表格放入selector的位置
 		,fillInto: function(selector){
 			var  elm = $(selector).eq(0)
