9f06847bffa69f03d5bd29721c553ac8a6741717
hisland <hisland@qq.com>
2011-09-16 15:56:46 +0800

    Color类更新

--- js/src/Color/Color.js
+++ js/src/Color/Color.js
@@ -2,91 +2,99 @@
  * 名称: 颜色类
  * 作者: hisland
  * 邮件: hisland@qq.com
- * 时间: 2010-8-11 12:7:19
- * 版本: v1
+ * 时间: 2011-09-16 15:43:43
+ * 版本: v2
  * 
- * 前置脚本:patch.javascript.js;
+ * API:
+ *		var c = Color('#ccff00');	//以hex方式创建
+ *		var c = Color('#f00');	//以hex缩写方式创建
+ *		var c = Color('rgb(200, 100, 50)');	//以rgb字符串方式创建,ff取出的颜色以此字符串返回
+ *		var c = Color('red');	//以颜色名方式创建,仅提供有限的几个
+ *		var c = Color(233, 20, 255);	//以rgb数字方式创建
  * 
- * 1个参数时:
- *		'#ffaa00'
- *		'#fa0'
- *		'rgb(15,16,17)'
- *		'rgb(15, 16, 17)' - 此为firefox生成颜色时的值,参数间有空格
- *		'red' - 常用的颜色名,下面的对应的列表,需要的可加
- * 3个参数时:
- *		155, 158, 138 - 应为正常的值,即每个值都在0-255间
- * 对象属性和方法:
- *		obj.toHex()可获得当前颜色的16进制表示:'#ffaa00'
- *		obj.toRgb()可获得当前颜色的16进制表示:'rgb(15,16,17)'
- *		obj.r\
- *		obj.g  --这3个分别对应颜色的RGB值
- *		obj.b/
+ *		c.toHex();	//取hex值
+ *		c.toRgb();	//取rgb值
+ * 
+ *		c.r;	//当前r值
+ *		c.g;	//当前g值
+ *		c.b;	//当前b值
+ * 
  */
 
-(function(){
+KISSY.add('Color', function(S, undef) {
 	//颜色名称对照表
 	var name_to_hex = {
-						 'red':'#ff0000'
-						,'green':'#008000'
-						,'blue':'#0000ff'
-						,'yellow':'#ffff00'
-						,'purple':'#800080'
-						,'gray':'#808080'
-						,'silver':'#c0c0c0'
-						,'pink':'#ffc0cb'
-						,'black':'#000000'
-						,'white':'#ffffff'
-						,'lime':'#00ff00'
-						,'aqua':'#00ffff'
-						,'fuchsia':'#ff00ff'
-						,'teal':'#008080'
-						,'maroon':'#800000'
-						,'olive':'#808000'
-					};
-	//内部解析hex值用的函数
-	function parseRgb(hex){
-		this.r = parseInt(hex.substr(1,2),16);
-		this.g = parseInt(hex.substr(3,2),16);
-		this.b = parseInt(hex.substr(5,2),16);
-	}
+		 'red':'#ff0000'
+		,'green':'#008000'
+		,'blue':'#0000ff'
+		,'yellow':'#ffff00'
+		,'purple':'#800080'
+		,'gray':'#808080'
+		,'silver':'#c0c0c0'
+		,'pink':'#ffc0cb'
+		,'black':'#000000'
+		,'white':'#ffffff'
+		,'lime':'#00ff00'
+		,'aqua':'#00ffff'
+		,'fuchsia':'#ff00ff'
+		,'teal':'#008080'
+		,'maroon':'#800000'
+		,'olive':'#808000'
+	};
+
+	
 	//颜色类,包含rgb值
-	function Color(){
-		var args = arguments, arr, aa, i;
+	function Color(arg0, arg1, arg2){
+		//更改为构造方式
+		if(!(this instanceof Color)){
+			return new Color(arg0, arg1, arg2);
+		}
+		var args = arguments, arr, tmp, i;
 
 		//一个参数的时候
 		if(args.length == 1){
 			arr = args[0].match(/(^#[\da-f]{6}$)|(^#[\da-f]{3}$)|(^rgb\((?:(?:\d{1,2}|[01]\d{2}|2[0-4]\d|25[0-5]),\s*){2}(?:\d{1,2}|[01]\d{2}|2[0-4]\d|25[0-5])\)$)/i);
-			//是#aaafff形式
-			if(arr && arr[1]){
-				parseRgb.call(this, arr[1]);
 
-			//是#f00形式
-			}else if(arr && arr[2]){
-				arr[2] = arr[2].replace(/([\da-f])/gi,'$1$1');
-				parseRgb.call(this, arr[2]);
+			if(arr){
+				//#aaafff形式
+				if(arr[1]){
+					this.__parseRgb(arr[1]);
+				}
 
-			//是rgb(255,255,255)形式
-			}else if(arr && arr[3]){
-				aa =arr[3].match(/\d{1,3}/g);
-				args.length = 3;
-				args[0] = parseInt(aa[0],10);
-				args[1] = parseInt(aa[1],10);
-				args[2] = parseInt(aa[2],10);
-			
+				//#f00形式
+				else if(arr[2]){
+					//转换成#ff0000形式
+					arr[2] = arr[2].replace(/([\da-f])/gi, '$1$1');
+					this.__parseRgb(arr[2]);
+				}
+
+				//rgb(255,255,255)形式
+				else if(arr[3]){
+					tmp =arr[3].match(/\d{1,3}/g);
+					args.length = 3;
+					args[0] = parseInt(tmp[0], 10);
+					args[1] = parseInt(tmp[1], 10);
+					args[2] = parseInt(tmp[2], 10);
+				}
+			}
+
 			//在名称对照表里存在
-			}else if(name_to_hex[args[0]]){
-				parseRgb.call(this, name_to_hex[args[0]]);
-			}else{
-				alert('颜色参数: '+args[0]+' 不正确,请检查!');
+			else if(name_to_hex[args[0]]){
+				this.__parseRgb(name_to_hex[args[0]]);
+			}
+
+			//其它情况
+			else{
+				console.log('Color: arguments ' + args[0] + ' invalid!');
 			}
 		}
 
 		//RGB3数字表示
 		if(args.length == 3){
-			for(i=0;i<args.length;i++){
+			for(i=0; i<3; i++){
 				if(args[i] > 255 || args[i] < 0){
-					alert('颜色参数: '+args[0]+','+args[1]+','+args[2]+' 不正确,请检查!');
-					return;
+					console.log('Color: arguments ' + args[0] + ', ' + args[1] + ', ' + args[2] + 'invalid!');
+					return null;
 				}
 			}
 			this.r=args[0];
@@ -97,29 +105,32 @@
 		return this;
 	}
 
-	//转换成HEX字符串#ff00cc
-	Color.prototype.toHex = function(){
-		var sr, sg, sb;
-		sr = this.r.toString(16);
-		sg = this.g.toString(16);
-		sb = this.b.toString(16);
-		if(sr.length==1){sr="0"+sr};
-		if(sg.length==1){sg="0"+sg};
-		if(sb.length==1){sb="0"+sb};
-		return "#"+sr+sg+sb;
-	}
+	S.augment(Color, {
+		//转换成HEX字符串#ff00cc
+		toHex: function(){
+			var sr, sg, sb;
+			sr = this.r.toString(16);
+			sg = this.g.toString(16);
+			sb = this.b.toString(16);
+			if(sr.length==1){sr="0" + sr};
+			if(sg.length==1){sg="0" + sg};
+			if(sb.length==1){sb="0" + sb};
+			return "#" + sr + sg + sb;
+		}
 
-	//转换成RGB字符串rgb(254,33,58)
-	Color.prototype.toRgb = function(){
-		return 'rgb('+this.r+','+this.g+','+this.b+')';
-	}
+		//转换成RGB字符串rgb(254,33,58)
+		,toRgb: function(){
+			return 'rgb(' + this.r + ', ' + this.g + ', ' + this.b + ')';
+		}
+
+		//内部解析hex值用的函数
+		,__parseRgb: function(hex){
+			this.r = parseInt(hex.substr(1,2), 16);
+			this.g = parseInt(hex.substr(3,2), 16);
+			this.b = parseInt(hex.substr(5,2), 16);
+		}
+	});
 
 	//导出为全局函数
-	window.Color = function(arg1, arg2, arg3){
-		if(arguments.length == 1){
-			return new Color(arg1);
-		}else if(arguments.length == 3){
-			return new Color(arg1, arg2, arg3);
-		}
-	}
-})();
+	window.Color = Color;
+});
