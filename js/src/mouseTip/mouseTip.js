accbedfe0652493ab4db10b6814e0aeb6fa45cd7
hisland <hisland@qq.com>
2011-09-19 16:36:45 +0800

    鼠标提示消息基本完成

--- js/src/mouseTip/mouseTip.js
+++ js/src/mouseTip/mouseTip.js
@@ -1,57 +1,97 @@
-(function($){
-	var  div_wrap = $('<div style="display:none;position:absolute;width:100px;height:50px;border:1px solid #afcdea;background:#f0f5fb;"></div>')
-		,tip = {};
+/**********************************************************************************************
+ * 名称: 鼠标移动提示
+ * 作者: hisland
+ * 邮件: hisland@qq.com
+ * 时间: 2011-09-19 09:33:38
+ * 版本: v1
+ * 
+ * API:
+ *		$(selector).mouseTip(msg);	//注册提示,使用固定消息
+ *		$(selector).mouseTip(fn);	//注册提示,使用函数返回值作为消息
+ * 
+ * 
+ */
 
-	function move(e){
+KISSY.add('mouseTip', function(S, undef) {
+	var  $ = jQuery
+		,div_wrap = $('<div style="display:none;position:absolute;width:100px;height:50px;border:1px solid #afcdea;background:#f0f5fb;"></div>');
+
+	//放入DOM
+	div_wrap.appendTo('body');
+
+	//鼠标移动时调整位置
+	function mouseMove(e){
 		var  body = document.documentElement
-			,scrollTop = body.scrollTop
-			,scrollLeft = body.scrollLeft
-			,maxWidth = body.clientWidth - 15
-			,maxHeight = body.clientHeight - 20
-			,offsetLeft, offsetTop;
+			,scroll_left = body.scrollLeft
+			,scroll_top = body.scrollTop
+			,max_width = body.clientWidth + scroll_left - 20
+			,max_height = body.clientHeight + scroll_top - 20
+			,pos_x, pos_y, elm_width, elm_height;
 
-		//修正鼠标相对窗口位置
+		//修正鼠标相对文档位置
 		if(e.pageX){
-			offsetLeft = e.pageX;
-			offsetTop = e.pageY;
+			pos_x = e.pageX;
+			pos_y = e.pageY;
 		}else{
-			offsetLeft = e.clientX + scrollLeft;
-			offsetTop = e.clientY + scrollTop;
+			pos_x = e.clientX + scroll_left;
+			pos_y = e.clientY + scroll_top;
 		}
 
+		//元素宽高
+		elm_width = div_wrap.outerWidth();
+		elm_height = div_wrap.outerHeight();
+
 		//设置水平对齐
-		if(maxWidth > offsetLeft+div_wrap.outerWidth()){
-			div_wrap.css('left', offsetLeft + 15);
+		if(max_width > pos_x + elm_width){
+			div_wrap.css('left', pos_x + 12);
 		}else{
-			div_wrap.css({left:'auto',right: maxWidth - offsetLeft + 16});
+			div_wrap.css('left', pos_x - elm_width);
 		}
 
 		//设置垂直对齐
-		if(maxHeight > offsetTop+div_wrap.outerHeight()){
-			div_wrap.css('top', offsetTop + 22);
+		if(max_height > pos_y + elm_height){
+			div_wrap.css('top', pos_y + 18);
 		}else{
-			div_wrap.css({top:'auto',bottom: maxHeight - offsetTop + 23});
+			div_wrap.css('top', pos_y - elm_height);
 		}
 	}
 
-	tip.show = function(e){
-		if(e){
-			move(e);
+	//鼠标进入
+	function enter(e){
+		var msg = $(this).data('mouseTip');
+		//根据函数计算显示值
+		if(S.isFunction(msg)){
+			div_wrap.html(msg.call($(this)));
 		}
+
+		//直接显示原始值
+		else{
+			div_wrap.html(msg);
+		}
+
+		//显示
 		div_wrap.show();
-		$(document).mousemove(move);
+		$(document).mousemove(mouseMove);
 	}
-	tip.hide = function(){
+
+	//鼠标离开隐藏
+	function leave(e){
 		div_wrap.hide();
-		$(document).unbind('mousemove', move);
+		$(document).unbind('mousemove', mouseMove);
 	}
 
-	tip.div = div_wrap;
-
-	//加载完之后添加到dom
-	$(function(){
-		div_wrap.appendTo('body');
+	$.fn.extend({
+		mouseTip: function(msg){
+			return this.each(function(i, v){
+				if(!this['--bind-mouseTip']){
+					this['--bind-mouseTip'] = true;
+					$(this).data('mouseTip', msg).hover(enter, leave);
+				}else{
+					S.log(this, 'has bind mouseTip!');
+				}
+			});
+		}
 	});
-
-	window.mouseTip = tip;
-})(jQuery);
+}, {
+	requires: ['jquery-1.4.2']
+});
