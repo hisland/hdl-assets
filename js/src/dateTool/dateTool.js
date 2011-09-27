6ed732bba8021a429c01784082aa33ad7c080aea
hisland <hisland@qq.com>
2011-09-27 21:47:37 +0800

    dateTool 跟进

--- js/src/dateTool/dateTool.js
+++ js/src/dateTool/dateTool.js
@@ -39,15 +39,6 @@ KISSY.add('dateTool', function(S, undef) {
 		msg_page_down = '下页',
 		msg_close = '关闭';
 
-	//reg_date共5种形式
-	// (^\d{4}\/\d{1,2}\/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}$) yyyy/mm/dd hh:mm:ss
-	// (^\d{4}\/\d{1,2}\/\d{1,2}$)	yyyy/mm/dd
-	// (^\d{1,2}:\d{1,2}:\d{1,2}$)	hh:mm:ss
-	// (^\d{4}\/\d{1,2}$)			yyyy/mm
-	// (^\d{1,2}\/\d{1,2}$)		mm/dd
-	var reg_date = /(^\d{4}\/\d{1,2}\/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}$)|(^\d{4}\/\d{1,2}\/\d{1,2}$)|(^\d{1,2}:\d{1,2}:\d{1,2}$)|(^\d{4}\/\d{1,2}$)|(^\d{1,2}\/\d{1,2}$)/;
-
-
 	function length1Prefix0(value) {
 		value += '';
 		return value.length == 1 ? '0' + value : value;
@@ -57,12 +48,19 @@ KISSY.add('dateTool', function(S, undef) {
 		return new Date(+new Date() + time_offset);
 	}
 
+	//reg_date共5种形式
+	// (^\d{4}\/\d{1,2}\/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}$) yyyy/mm/dd hh:mm:ss
+	// (^\d{4}\/\d{1,2}\/\d{1,2}$)	yyyy/mm/dd
+	// (^\d{1,2}:\d{1,2}:\d{1,2}$)	hh:mm:ss
+	// (^\d{4}\/\d{1,2}$)			yyyy/mm
+	// (^\d{1,2}\/\d{1,2}$)		mm/dd
+	var reg_date = /(^\d{4}\/\d{1,2}\/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}$)|(^\d{4}\/\d{1,2}\/\d{1,2}$)|(^\d{1,2}:\d{1,2}:\d{1,2}$)|(^\d{4}\/\d{1,2}$)|(^\d{1,2}\/\d{1,2}$)/;
 	function parseValueToDate(value, time_offset){
-		var  arr = value.replace(/-/g,'/').match(reg_date)
-			,date_now = now(time_offset)
-			,date_obj;
+		var arr = value.replace(/-/g,'/').match(reg_date),
+			date_now = now(time_offset),
+			date_obj = date_now;
 
-		//没有的时候,不做操作,返回undefined
+		//没有的时候,不做操作,返回当前时间
 		if(!arr){
 
 		//"2010/05/19 16:02:26"的形式 == 年/月/日 时:分:秒
@@ -95,6 +93,8 @@ KISSY.add('dateTool', function(S, undef) {
 		if(!(this instanceof DateSetting)){
 			return new DateSetting();
 		}
+
+		this.fixed = '';
 	}
 	S.mix(DateSetting, {
 		reg_fixed: /(year|month|date|hour|minute|second)([\de]*)/g,
@@ -160,30 +160,16 @@ KISSY.add('dateTool', function(S, undef) {
 				this.item('hour', date.getHours());
 				this.item('minute', date.getMinutes());
 				this.item('second', date.getSeconds());
-				this.date = date;
+				this.__date = date;
 			}
 			return this;
 		},
 		getDate: function(){
-			var i, arr, str, arr2=[];
-			arr = 'year,month,date'.split(',');
-			for(i=0;i<3;i++){
-				arr2.push(this.real(arr[i]));
-			}
-			str = arr2.join('/');
-
-			arr = 'hour,minute,second'.split(',');
-			arr2.length = 0;
-			for(i=0;i<3;i++){
-				arr2.push(this.real(arr[i]));
-			}
-			str += ' ' + arr2.join(':');
-
-			return new Date(str);
+			return this.__data;
 		},
-		refreshFixed: function(fixed){
+		refreshFixed: function(){
 			var i, v, o = {}, $ipt_list = $ipt_year.parent();
-			fixed.replace(DateSetting.reg_fixed, function(a, b, c){
+			this.fixed.replace(DateSetting.reg_fixed, function(a, b, c){
 				if(b && !o[b]){
 					o[b] = c;
 				}
@@ -196,7 +182,6 @@ KISSY.add('dateTool', function(S, undef) {
 					this[i] = v;
 				}
 			}
-			this.fixed = o;
 			return this;
 		},
 		refreshDataList: function(itemFilter){
@@ -208,7 +193,7 @@ KISSY.add('dateTool', function(S, undef) {
 			for(i = 1; i < firstDay; i++) {
 				str.push('<span></span>');
 			}
-			if(_date_disabled || typeof itemFilter === 'function'){
+			if(_date_disabled || S.isFunction(itemFilter)){
 				for(i = 1; i <= days; i++) {
 					if(_date_disabled || !itemFilter(i)){
 						str.push('<strong>' + i + '</strong>');
@@ -263,7 +248,7 @@ KISSY.add('dateTool', function(S, undef) {
 				$drop_prev.add($drop_next).css('visibility', 'hidden');
 			}
 
-			if(typeof itemFilter === 'function'){
+			if(S.isFunction(itemFilter)){
 				for( ; from <= to ; from++ ){
 					if(itemFilter(from)){
 						if(from !== now){
@@ -288,8 +273,6 @@ KISSY.add('dateTool', function(S, undef) {
 			return this;
 		},
 		refreshIpts: function(type){
-			var arr, i;
-			
 			//refresh specify type
 			if(type === 'year'){
 				$ipt_year.val(this.item(type));
@@ -311,8 +294,8 @@ KISSY.add('dateTool', function(S, undef) {
 			
 			//refresh all
 			else{
-				arr = 'year,month,date,hour,minute,second'.split(',');
-				for (i = 0; i < arr.length; i++) {
+				var arr = 'year,month,date,hour,minute,second'.split(','), i = 0;
+				for (; i < arr.length; i++) {
 					this.refreshIpts(arr[i]);
 				}
 			}
@@ -357,29 +340,19 @@ KISSY.add('dateTool', function(S, undef) {
 	function dropNext(){
 		setting.real('year', ($div_drop_list.find('a:last').html()-0)+1).refreshDropList('year');
 	}
-	function dropSelect(dt){
+	function dropSelect($t){
 		var type = $target_ipt[0].className.substring(4);
-		setting.item(type, dt.html()).refreshIpts(type);
-		dt.addClass('hdt-drop-list-now').siblings('a.hdt-drop-list-now').removeClass('hdt-drop-list-now');
+		setting.item(type, $t.html()).refreshIpts(type);
+		$t.addClass('hdt-drop-list-now').siblings('a.hdt-drop-list-now').removeClass('hdt-drop-list-now');
 		if(type === 'year' || type === 'month' || type === 'date'){
 			setting.refreshIpts('date').refreshDataList();
 		}
 		setting.fillTarget();
 		dropClose();
 	}
-	function dateSelect(dt){
-		setting.item('date', dt.html()).refreshIpts('date').fillTarget();
-		dt.addClass('hdt-date-now').siblings('a.hdt-date-now').removeClass('hdt-date-now');
-	}
-
-	function toolOpen(target){
-		$target_fill = target;
-		$div_wrap.adjustElement(target).show();
-	}
-	function toolClose(){
-		dropClose();
-		$target_fill = $EMPTY;
-		$div_wrap.hide();
+	function dateSelect($t){
+		setting.item('date', $t.html()).refreshIpts('date').fillTarget();
+		$t.addClass('hdt-date-now').siblings('a.hdt-date-now').removeClass('hdt-date-now');
 	}
 
 	function btnClearClick(){
@@ -396,18 +369,16 @@ KISSY.add('dateTool', function(S, undef) {
 
 
 	function divWrapClick(e){
-		var	 t = e.target
-			,dt = $(t)
-			,tag_name = t.tagName.toUpperCase()
-			,parent_class = t.parentNode.className;
-		if(tag_name === 'INPUT' && !t.disabled){
-			$target_ipt = dt;
+		var t = e.target,
+			$t = $(t);
+		if($t.is('input') && !t.disabled){
+			$target_ipt = $t;
 			dropOpen();
-		}else if(tag_name === 'A'){
-			if(dt.closest('.hdt-drop-list').length){
-				dropSelect(dt);
-			}else if(dt.closest('.hdt-date-list').length){
-				dateSelect(dt);
+		}else if($t.is('a')){
+			if($t.closest('.hdt-drop-list').length){
+				dropSelect($t);
+			}else if($t.closest('.hdt-date-list').length){
+				dateSelect($t);
 			}else if(t === $btn_clear[0]){
 				btnClearClick();
 			}else if(t === $btn_now[0]){
@@ -422,19 +393,36 @@ KISSY.add('dateTool', function(S, undef) {
 				dropNext();
 			}
 			e.preventDefault();
-		}else if(!dt.closest('.hdt-drop-list-wrap').length){
+		}else if(!$t.closest('.hdt-drop-list-wrap').length){
 			dropClose();
 		}
 	}
 	function divWrapDblclick(e){
-		if(e.target.tagName.toUpperCase() === 'A' && e.target.parentNode.className === 'hdt-date-list'){
+		if($(e.target).is('a') && $(e.target).parent().is('.hdt-date-list')){
 			toolClose();
+		}
+	}
+	function divWrapWheel(e, delta){
+		var t = e.target,
+			$t = $(t);
+		if($div_wrap.find('.hdt-drop-list').is(':visible')){
+			if(delta < 0){
+				$div_wrap.find('.hdt-drop-now').next('a').click();
+			}else{
+				$div_wrap.find('.hdt-drop-now').prev('a').click();
+			}
+		}else{
+			if(delta < 0){
+				$div_wrap.find('.hdt-date-now').next('a').click();
+			}else{
+				$div_wrap.find('.hdt-date-now').prev('a').click();
+			}
 		}
 	}
 
 
 	//初始化DOM结构, 各个节点的引用
-	$div_wrap = $('<div class="hdt-wrap"><div class="hdt-ctrl"><div class="hdt-tips"></div><div class="hdt-btns"><a href="#" class="hdt-clear">清除</a><a href="#" class="hdt-now">现在</a><a href="#" class="hdt-complete">完成</a></div></div><div class="hdt-ipt-list"><input type="text" class="hdt-year" />-<input type="text" class="hdt-month" />-<input type="text" class="hdt-date" /><input type="text" class="hdt-hour" />:<input type="text" class="hdt-minute" />:<input type="text" class="hdt-second" /></div><div class="hdt-week-list"></div><div class="hdt-date-list"></div><div class="hdt-drop-list-wrap"><div class="hdt-drop-list"></div><p class="hdt-drop-list-ctrl"><a href="#">上页</a><a href="#">下页</a><a href="#">关闭</a></p></div></div>');
+	$div_wrap = $('<div class="hdt-wrap"><div class="hdt-ctrl"><div class="hdt-tips"></div><div class="hdt-btns"><a href="#" class="hdt-clear">清除</a><a href="#" class="hdt-now">现在</a><a href="#" class="hdt-complete">完成</a></div></div><div class="hdt-ipt-list"><input type="text" class="hdt-year" />-<input type="text" class="hdt-month" />-<input type="text" class="hdt-date" /><input type="text" class="hdt-hour" />:<input type="text" class="hdt-minute" />:<input type="text" class="hdt-second" /></div><div class="hdt-week-list"><span>一</span><span>二</span><span>三</span><span>四</span><span>五</span><span>六</span><span>日</span></div><div class="hdt-date-list"></div><div class="hdt-drop-list-wrap"><div class="hdt-drop-list"></div><p class="hdt-drop-list-ctrl"><a href="#">上页</a><a href="#">下页</a><a href="#">关闭</a></p></div></div>');
 
 	$btn_clear = $div_wrap.find('a.hdt-clear');
 	$btn_now = $btn_clear.next();
@@ -468,9 +456,21 @@ KISSY.add('dateTool', function(S, undef) {
 
 	//放入body
 	$div_wrap.appendTo('body');
-	$div_wrap.click(divWrapClick).dblclick(divWrapDblclick);
+	//事件代理
+	$div_wrap.click(divWrapClick).dblclick(divWrapDblclick).mousewheel(divWrapWheel);
 
 
+	function toolOpen(target){
+		$target_fill = $(target);
+		setting = target.date_setting;
+		setting.refreshFixed().refreshIpts().refreshDataList();
+		$div_wrap.adjustElement(target).show();
+	}
+	function toolClose(){
+		dropClose();
+		$target_fill = $EMPTY;
+		$div_wrap.hide();
+	}
 	//输入框获得焦点显示
 	function iptFocus(e){
 		if(this.disabled){// || this.date_setting.disabled
@@ -496,6 +496,7 @@ KISSY.add('dateTool', function(S, undef) {
 				if(!this['--bind-dateTool']){
 					this['--bind-dateTool'] = true;
 					this.date_setting = DateSetting();
+					this.date_setting.setDate(parseValueToDate(this.value));
 					$(this).focus(iptFocus);
 				}
 			});
