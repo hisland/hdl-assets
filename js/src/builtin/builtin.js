faaa8ca0689d67299e3d75f53fc03a9ae4071b23
hisland <hisland@qq.com>
2011-10-13 19:11:33 +0800

    增加 查看json 只查看第一级,不递归

--- js/src/builtin/builtin.js
+++ js/src/builtin/builtin.js
@@ -16,6 +16,8 @@ function __escapeDashQuote(str){
 		}
 	});
 }
+
+//递归生成json
 function viewJSON(obj, tabs){
 	var isArr = Object.prototype.toString.apply(obj) === '[object Array]',
 		bracket  = isArr ? '[' : '{',
@@ -50,6 +52,60 @@ function viewJSON(obj, tabs){
 
 		else if(type === 'object'){
 			buff.push((isArr ? '' : '\n'+indent+'"'+i+'":') + viewJSON(tmp,indent));
+		}
+
+		else if(type === 'function'){
+			buff.push('\n' + indent + (isArr ? '"' : '"'+i+'":"') + '[function]"');
+		}
+
+		else if(tmp === undefined){
+			buff.push('\n' + indent + (isArr ? '' : '"'+i+'":') + 'undefined');
+		}
+
+		else{
+			buff.push('\n' + indent + (isArr ? '"' : '"'+i+'":"') + '[unKnownType]"');
+		}
+	}
+	bracket += buff.join(',');
+	bracket += '\n' + tabs + (isArr ? ']' : '}');
+	return bracket ;
+}
+
+//只查看第一级,不递归
+function viewJSON1(obj, tabs){
+	var isArr = Object.prototype.toString.apply(obj) === '[object Array]',
+		bracket  = isArr ? '[' : '{',
+		buff = [], i, type, tmp;
+	tabs = tabs || '';
+	var indent = tabs + '\t';
+	for(i in obj){
+		//只显示自己的属性,不显示原型链上的属性
+		if (!obj.hasOwnProperty(i)){
+			continue;
+		}
+
+		tmp = obj[i];
+		type = typeof tmp;
+		
+		if(type === 'number'){
+			buff.push('\n' + indent + (isArr ? '' : '"'+i+'":') + tmp);
+		}
+
+		else if(type === 'string'){
+			buff.push('\n' + indent + (isArr ? '"' : '"'+i+'":"') + __escapeDashQuote(tmp) + '"');
+		}
+
+		else if(type === 'boolean'){
+			buff.push('\n' + indent + (isArr ? '' : '"'+i+'":') + tmp + '');
+		}
+
+		//typeof null === 'object', so check the real value first
+		else if(tmp === null){
+			buff.push('\n' + indent + (isArr ? '' : '"'+i+'":') + 'null');
+		}
+
+		else if(type === 'object'){
+			buff.push('\n' + indent + (isArr ? '"' : '"'+i+'":"') + '[object]"');
 		}
 
 		else if(type === 'function'){
