5ecd88a57800a2a30771f7e04aa5aaa1294a18fe
hisland <hisland@qq.com>
2011-10-13 20:14:20 +0800

    增加 文本框修改时的事件 input

---
+++ js/src/jquery.input/jquery.input.js
@@ -0,0 +1,68 @@
+/**********************************************************************************************
+ * 名称: 输入框内容变化时触发的事件
+ * 作者: hisland
+ * 邮件: hisland@qq.com
+ * 时间: @TIMESTAMP@
+ * 版本: @VERSION@
+ * 
+ * NOTICE:
+ *		由于输入框文本改变方式有多种(键盘输入,右键操作...)
+ *		IE使用propertychange事件
+ *		FF使用input事件
+ * 
+ * API:
+ *		$(':text').bind('input', fn);
+ * 
+ */
+
+KISSY.add('jquery.input', function(S, undef) {
+
+var types = ['input', 'propertychange'];
+
+$.event.special.input = {
+	setup: function() {
+		if ( this.addEventListener )
+			for ( var i=types.length; i; )
+				this.addEventListener( types[--i], handler, false );
+		else
+			this.onmousewheel = handler;
+	},
+	
+	teardown: function() {
+		if ( this.removeEventListener )
+			for ( var i=types.length; i; )
+				this.removeEventListener( types[--i], handler, false );
+		else
+			this.onmousewheel = null;
+	}
+};
+
+$.fn.extend({
+	mousewheel: function(fn) {
+		return fn ? this.bind("mousewheel", fn) : this.trigger("mousewheel");
+	},
+	
+	unmousewheel: function(fn) {
+		return this.unbind("mousewheel", fn);
+	}
+});
+
+
+function handler(event) {
+	var args = [].slice.call( arguments, 1 ), delta = 0, returnValue = true;
+	
+	event = $.event.fix(event || window.event);
+	event.type = "mousewheel";
+	
+	if ( event.wheelDelta ) delta = event.wheelDelta/120;
+	if ( event.detail     ) delta = -event.detail/3;
+	
+	// Add events and delta to the front of the arguments
+	args.unshift(event, delta);
+
+	return $.event.handle.apply(this, args);
+}
+
+}, {
+	requires: ['jquery-1.4.2']
+});
