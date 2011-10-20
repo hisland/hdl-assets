334e843bc74582ad7c7afce3360e06d00ba21b29
hisland <hisland@qq.com>
2011-10-20 16:45:44 +0800

    add script to make a new mod quickly

---
+++ js/src/mod-template/mod-template.js
@@ -0,0 +1,19 @@
+/**********************************************************************************************
+ * 名称: mod-template
+ * 作者: hisland
+ * 邮件: hisland@qq.com
+ * 时间: @TIMESTAMP@
+ * 版本: @VERSION@
+ * 
+ * NOTICE:
+ * 
+ * API:
+ * 
+ */
+
+KISSY.add('mod-template', function(S, undef) {
+	var $ = jQuery;
+
+}, {
+	requires: ['jquery-1.4.2']
+});
---
+++ js/src/new-mod.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	echo 'please enter a mod name!'
+	exit 1
+fi
+
+if [ -e $1 ]
+then
+	echo "[ $1 ] has already exist!"
+	exit 1
+fi
+
+cp -r mod-template $1
+cd $1
+
+mkdir misc $1-img
+
+sed "s/mod-template/$1/" build.properties > tmp
+mv tmp build.properties
+
+sed "s/mod-template/$1/" build.xml > tmp
+mv tmp build.xml
+
+sed "s/mod-template/$1/" mod-template.js > tmp
+mv tmp mod-template.js
+
+sed "s/mod-template/$1/" demo.html > tmp
+mv tmp demo.html
+
+sed "s/mod-template/$1/" view.html > tmp
+mv tmp view.html
+
+mv mod-template.css $1.css
+mv mod-template.js $1.js
+
+echo 'mod create ok!'
