d83adf04aa6dfb1827347c6d5d850ff4978b2757
hisland <hisland@qq.com>
2011-11-01 16:21:23 +0800

    add ant target jar-src to package the sources

---
+++ java/tag/make-jar-src.bat
@@ -0,0 +1,9 @@
+call ant jar-src
+echo.
+echo --------------------------------------------------
+echo    jar with src!
+echo    please find the latest jar file in build dir!
+echo --------------------------------------------------
+echo.
+pause
+exit
