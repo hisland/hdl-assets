d1a2ce4bfc3c0b28195d231cb80bba869968e2a9
hisland <hisland@qq.com>
2011-12-26 17:00:39 +0800

    change mouseTip

--- js/src/mouseTip/mouseTip.js
+++ js/src/mouseTip/mouseTip.js
@@ -1,23 +1,19 @@
-/**********************************************************************************************
- * 名称: 鼠标移动提示
- * 作者: hisland
- * 邮件: hisland@qq.com
- * 时间: 2011-09-19 09:33:38
- * 版本: v1
- * 
+/**
+ * @fileOverview
+ * @module mouseTip
+ * @author hisland hisland@qq.com
+ * @description 鼠标移动提示
+ * <pre><code>
  * API:
- *		$(selector).mouseTip(msg);	//注册提示,使用固定消息
- *		$(selector).mouseTip(fn);	//注册提示,使用函数返回值作为消息
- * 
- * 
+ *		$(selector).mouseTip(msg);		//注册/修改提示,使用固定消息
+ *		$(selector).mouseTip(fn);		//注册/修改提示,使用函数返回值作为消息
+ *		$(selector).mouseTip(false);	//关闭鼠标移动提示
+ * </code></pre>
  */
 
 KISSY.add('mouseTip', function(S, undef) {
 	var $ = jQuery,
-		div_wrap = $('<div style="display:none;position:absolute;width:100px;height:50px;border:1px solid #afcdea;background:#f0f5fb;"></div>');
-
-	//放入DOM
-	div_wrap.appendTo('body');
+		$div = $('<div style="display:none;position:absolute;width:100px;height:50px;padding:3px;border:1px solid #afcdea;background:#f0f5fb;"></div>');
 
 	//鼠标移动时调整位置
 	function mouseMove(e){
@@ -38,21 +34,21 @@ KISSY.add('mouseTip', function(S, undef) {
 		}
 
 		//元素宽高
-		elm_width = div_wrap.outerWidth();
-		elm_height = div_wrap.outerHeight();
+		elm_width = $div.outerWidth();
+		elm_height = $div.outerHeight();
 
 		//设置水平对齐
 		if(max_width > pos_x + elm_width){
-			div_wrap.css('left', pos_x + 12);
+			$div.css('left', pos_x + 12);
 		}else{
-			div_wrap.css('left', pos_x - elm_width);
+			$div.css('left', pos_x - elm_width);
 		}
 
 		//设置垂直对齐
 		if(max_height > pos_y + elm_height){
-			div_wrap.css('top', pos_y + 18);
+			$div.css('top', pos_y + 18);
 		}else{
-			div_wrap.css('top', pos_y - elm_height);
+			$div.css('top', pos_y - elm_height);
 		}
 	}
 
@@ -61,33 +57,44 @@ KISSY.add('mouseTip', function(S, undef) {
 		var msg = $(this).data('mouseTip');
 		//根据函数计算显示值
 		if(S.isFunction(msg)){
-			div_wrap.html(msg.call($(this)));
+			$div.html(msg.call($(this)));
 		}
-
 		//直接显示原始值
 		else{
-			div_wrap.html(msg);
+			$div.html(msg);
 		}
 
 		//显示
-		div_wrap.show();
+		$div.show();
 		$(document).mousemove(mouseMove);
 	}
 
 	//鼠标离开隐藏
 	function leave(e){
-		div_wrap.hide();
+		$div.hide();
 		$(document).unbind('mousemove', mouseMove);
 	}
+
+	//放入DOM
+	$div.appendTo('body');
 
 	$.fn.extend({
 		mouseTip: function(msg){
 			return this.each(function(i, v){
-				if(!this['--bind-mouseTip']){
-					this['--bind-mouseTip'] = true;
-					$(this).data('mouseTip', msg).hover(enter, leave);
-				}else{
-					S.log(this, 'has bind mouseTip!');
+				//取消提示
+				if(msg === false){
+					this['--bind-mouseTip'] = false;
+					$(this).removeData('mouseTip').unbind('mouseenter', enter).unbind('mouseleave', leave);
+				}
+				//添加或修改提示内容
+				else{
+					$(this).data('mouseTip', msg);
+
+					//只绑定一次事件
+					if(!this['--bind-mouseTip']){
+						this['--bind-mouseTip'] = true;
+						$(this).hover(enter, leave);
+					}
 				}
 			});
 		}
