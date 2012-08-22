f7351196979f0a35bcff0812233d990a647f2cec
hisland <hisland@qq.com>
2012-08-22 16:05:11 +0800

    calendar2 almost complete the image and css

---
+++ js/requirejs/ui/calendar2/config.js
@@ -0,0 +1,22 @@
+define({
+	/** 是否选中后就关闭 */
+	selectHide : false,
+	/** 是否开启动画 */
+	enableAnimate : false,
+	/** 是否开启动画 */
+	enableAnimate : false,
+	/** 是否开启动画 */
+	enableAnimate : false,
+	/** 是否开启动画 */
+	enableAnimate : false,
+	/** 是否开启动画 */
+	enableAnimate : false,
+	/** 是否开启动画 */
+	enableAnimate : false,
+	/** 是否开启动画 */
+	enableAnimate : false,
+	/** 是否开启动画 */
+	enableAnimate : false,
+	/** 是否开启动画 */
+	enableAnimate : false
+});
