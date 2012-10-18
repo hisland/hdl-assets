1b2e22f88d8838bd0e5be2a3b964819e2ad224a2
hisland <hisland@qq.com>
2012-10-18 18:29:58 +0800

    grid2 has some change , that is good

--- js/requirejs/ui/grid2/grid.js
+++ js/requirejs/ui/grid2/grid.js
@@ -1,71 +1,82 @@
 define(['jquery', 'kissy', 'util'], function($, S, Util){
 
-var i18n = {
-	week: ['', '一', '二', '三', '四', '五', '六', '日'],
-	clear: '清除',
-	now: '现在',
-	close: '关闭',
-	prev: '前一页',
-	next: '后一页'
-};
 var defaultConfig = {
-	enableDetail : false,
-	autoRefresh : {
+	enableDetail: false,
+	autoRefresh: {
 		timeout: 5000,
 		process: function(){
 			
 		}
 	},
-	ajax : {
+	ajax: {
 		url: '',
 		method: 'POST',
 		dataType: 'json',
 		param: null,
 		lastParam: null
 	},
-	preProcess : null,
-	theme : 'default',
-	enablePager: true,
-	pagerTheme : 'default',
+	preProcess: null,
+	theme: 'default',
+
+	enablePage: true,
+	pageTheme: 'default',
 	perPageNum: 15,
 	perPageNumList: [8,10,15,20,25,40],
+
 	enableCheckbox: true,
 	checkboxFixto: null,
+	clickCheck: true,//点击行便选中checkobx
 	singleCheck: false,
+
 	enableMask: true,
-	nowrap: true,
-	enableColToggle : true,
-	minColToggle : 1,
+	nowrap: true,//不换行
+	cellWrap: false,//是否单元格用div包裹起来
+
+	enableFixCol: false,
+
+	enableColToggle: true,
+	minColToggle: 1,
+
+	width: 'auto',
+	height: 'auto',
+
 	autoResize: false,
 	autoload: false
-};
-var defaultResult = {
-	perPageNum : 15,
-	currPage : 1,
-	beginNum : 0,
-	endNum : 0,
-	totals : 0,
-	currRecordNum : 0,
-	allPage : 0,
-	rows : []
-};
-var defaultRow = {
-	enableCheck: true,
-	disabled: false
 };
 var defaultCol = {
 	align: 'left',//left right center
 	alignHead: 'center',
-	fixedTo: null,//left right null
-	width: 'auto',//auto 15% 15
+
+	fixTo: null,//left right null
+
+	width: 'auto',//'auto' '15%' 15
 	minWidth: 10,
 	maxWidth: 200,
-	headText: 'col Head',
+
+	headText: 'Head Text',
+	textStyle: null,
+
 	hide: false,
 	enableHide: false,
+
 	enableSort: false,
 	enableTip: false,
 	process: null
+};
+var defaultRow = {
+	align: null,
+	enableCheck: true,
+	disabled: false
+};
+var defaultResult = {
+	perPageNum: 15,
+	currPage: 1,
+	beginNum: 0,
+	endNum: 0,
+	totals: 0,
+	currRecordNum: 0,
+	allPage: 0,
+	rows: []
 };
 
 
@@ -110,10 +121,14 @@ $.extend(Grid.prototype, {
 
 //宽高
 $.extend(Grid.prototype, {
-	setHeight: function(num){
+	setWidth: function(num){
 		return this;
 	},
-	setWidth: function(num){
+	setHeight: function(num){
+		this.setBodyHeight();
+		return this;
+	},
+	setBodyHeight: function(num){
 		return this;
 	}
 });
