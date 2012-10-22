48e5695dcfbf420a10e2763052b57ad3803f35a8
hisland <hisland@qq.com>
2012-10-22 09:16:13 +0800

    grid2 change

--- js/requirejs/ui/grid2/grid.js
+++ js/requirejs/ui/grid2/grid.js
@@ -8,13 +8,12 @@ var defaultConfig = {
 			
 		}
 	},
-	ajax: {
-		url: '',
-		method: 'POST',
-		dataType: 'json',
-		param: null,
-		lastParam: null
-	},
+
+	url: '',
+	data: null,
+	lastData: null,
+	delay: 50,//延迟加载,避免一直按住回车不停请求,只加载最后一次
+
 	preProcess: null,
 	theme: 'default',
 
@@ -44,10 +43,10 @@ var defaultConfig = {
 	autoload: false
 };
 var defaultCol = {
-	align: 'left',//left right center
-	alignHead: 'center',
+	align: 'left',//'left' 'right' 'center'
+	alignHead: 'center',//'left' 'right' 'center'
 
-	fixTo: null,//left right null
+	fixTo: null,//'left' 'right' 'null'
 
 	width: 'auto',//'auto' '15%' 15
 	minWidth: 10,
@@ -56,11 +55,15 @@ var defaultCol = {
 	headText: 'Head Text',
 	textStyle: null,
 
+	enableHide: true,
 	hide: false,
-	enableHide: false,
 
 	enableSort: false,
+	sortType: 'alpha', //'number' 'alpha'
+
 	enableTip: false,
+	tipType: 'default',//'default' 'mouse'
+
 	process: null
 };
 var defaultRow = {
@@ -81,40 +84,37 @@ var defaultResult = {
 
 
 function Grid(config){
-	this.__init(config).__initDOM().__initEvent();
+	this.__initConfig(config).__initDOM().__initEvent().__init();
 }
 
 //初始化, 事件
 $.extend(Grid.prototype, {
-	__init: function(config){
+	__initConfig: function(config){
 		config = $.isPlainObject(config) ? config : {};
-		this.config = $.extend(true, config, defaultConfig);
+		this.config = S.mix(config, defaultConfig, false);
+		S.each(config.colModel, function(v, i, o){
+			$.extend(true, v, defaultCol);
+		});
 		return this;
 	},
 	__initDOM: function(){
-		var div = $('');
+		var div = $('<div class="grid-wrap"><div class="grid-title-wrap"></div><div class="grid-button-wrap"></div><div class="grid-table-wrap nofixcol"><div class="grid-table table-left"></div><div class="grid-table table-center"><div class="grid-head-wrap"><table><thead></thead></table></div><div class="grid-body-wrap"><table><tbody></tbody></table></div></div><div class="grid-table table-right"></div><div class="clear-left"></div></div><div class="grid-page-wrap"></div></div>');
 		this.$wrap = div;
-		div.appendTo('body');
+		this.$titleDiv = div.find('div.grid-title-wrap');
+		this.$buttonDiv = div.find('div.grid-button-wrap');
+		this.$tableWrapDiv = div.find('div.grid-table-wrap');
+		this.$centerHeadDiv = div.find('div.table-center>.grid-head-wrap');
+		this.$centerTHead = this.$centerHeadDiv.find('thead');
+		this.$centerBodyDiv = div.find('div.table-center>.grid-body-wrap');
+		this.$centerTBody = this.$centerBodyDiv.find('tbody');
+		this.$pageDiv = div.find('div.grid-page-wrap');
 		return this;
 	},
 	__initEvent: function(){
 		var me = this;
 		return this;
-	}
-});
-
-//事件
-$.extend(Grid.prototype, {
-	on: function(type, fn){
-		$(this).on(type, fn);
-		return this;
 	},
-	off: function(type, fn){
-		$(this).off(type, fn);
-		return this;
-	},
-	trigger: function(type){
-		$(this).trigger(type);
+	__init: function(){
 		return this;
 	}
 });
@@ -133,15 +133,147 @@ $.extend(Grid.prototype, {
 	}
 });
 
+//生成各种内容
+$.extend(Grid.prototype, {
+	makeCol: function(){
+		return this;
+	},
+	makeHead: function(){
+		return this;
+	},
+	makeBoby: function(){
+		return this;
+	},
+	makePage: function(){
+		return this;
+	},
+	makeHTML: function(){
+		return this;
+	},
+	calcData: function(){
+		var cols = this.colModel, data = this.data;
+		this.$tmpTable.html();
+		return this;
+	},
+	calcDimension: function(){
+		var width = [], height = [];
+		this.$tmpTable.find('col').each(function(i, v){
+			width.push($(v).width());
+		});
+		this.$tmpTable.find('tbody tr').each(function(i, v){
+			height.push($(v).height());
+		});
+
+		this.__width = width;
+		this.__height = height;
+		return this;
+	}
+});
+
 //设置数据
 $.extend(Grid.prototype, {
 	setData: function(data){
+		if($.isPlainObject(data)){
+			this.data = data;
+		}else{
+			throw {
+				message: 'error: Grid.setData:',
+				data: data
+			}
+		}
 		return this;
 	},
-	setHTML: function(obj){
+	setHTMLData: function(obj){
 		obj.col;
 		obj.head;
 		obj.body;
+		return this;
+	},
+	ajaxLoad: function(fn){
+		this.__delay && this.__delay.cancel();
+		this.__req && this.__req.abort();
+
+		this.__delay = S.later(function(){
+			this.__delay = null;
+
+			this.loading();
+			this.__req = $.ajax({
+				url: this.config.url,
+				data: this.config.data,
+				dataType: 'json',
+				context: this,
+				success: function(data){
+					this.setData(data);
+					$.isFunction(fn) && fn();
+					this.loaded();
+				}
+			});
+		}, this.delay, false, this);
+		return this;
+	},
+	render: function(){
+		return this;
+	},
+	blankLine: function(){
+		var str = ['<tr style="visibility:hidden;">'];
+
+		//checkbox需要占一列
+		if(this.enableCheckbox){
+			str.push('<td></td>');
+		}
+
+		//根据colModel计算需要多少列
+		str.push(S.map(this.colModel, function(v, i, o){
+			return '<td></td>';
+		}).join(''), '</tr>');
+
+		this.$centerTBody.html(str.join(''));
+		return this;
+	}
+});
+
+//加载状态
+$.extend(Grid.prototype, {
+	loading: function(){
+		this.__loading = true;
+		return this;
+	},
+	loaded: function(){
+		this.__loading = false;
+		this.__req = null;
+		return this;
+	}
+});
+
+//加载分页
+$.extend(Grid.prototype, {
+	loadPage: function(n){
+		if(n >= 1 && n <= this.data.allPage){
+			this.data.currPage = n;
+			this.ajaxLoad();
+		}else{
+			S.log(['error: Grid.loadPage: ', n]);
+		}
+		return this;
+	},
+	refresh: function(){
+		this.loadPage(this.data.currPage);
+		return this;
+	},
+	nextPage: function(){
+		this.loadPage(this.data.currPage + 1);
+		return this;
+	},
+	prevPage: function(){
+		this.loadPage(this.data.currPage - 1);
+		return this;
+	},
+	firstPage: function(){
+		this.loadPage(1);
+		return this;
+	},
+	lastPage: function(){
+		this.loadPage(this.data.allPage);
 		return this;
 	}
 });
