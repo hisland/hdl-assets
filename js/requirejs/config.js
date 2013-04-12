240cbf91a034760d4a5e51883f6d2ebe00e0de24
hisland <hisland@qq.com>
2013-04-12 14:16:30 +0800

    update libs

--- js/requirejs/config.js
+++ js/requirejs/config.js
@@ -14,5 +14,29 @@ require.config({
 	shim: {
 		'jquery-ui': ['jquery'],
 		'bootstrap': ['jquery']
-	}
+	},
+	packages: ['css',
+				'demo',
+				'jquery-plugin',
+				'util',
+				'validator',
+				'ui/ajax-tab',
+				'ui/autocomp',
+				'ui/button',
+				'ui/calendar',
+				'ui/drag-sort',
+				'ui/grid',
+				'ui/highcharts',
+				'ui/pager',
+				'ui/popbox',
+				'ui/pop-manager',
+				'ui/popup',
+				'ui/popwin',
+				'ui/select',
+				'ui/swfobject',
+				'ui/tab',
+				'ui/tip',
+				'ui/tree',
+				'ui/table-tree',
+				'ui/week']
 });
--- js/requirejs/ui-new/button/initEvent.js
+++
@@ -1,8 +0,0 @@
-define(['jquery', 'kissy', 'jquery-plugin'], function($, S){
-	return function(o){
-
-		o.$div.on('click', o.config.click);
-
-		return o;
-	};
-});
